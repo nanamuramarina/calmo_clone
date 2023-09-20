8d1480ae996fbaf44b3713be6c148fc34c665c39
nanamuramarina <nanamuramarina@gmail.com>
2023-09-20 12:58:25 +0000

    [fix]検索機能、レイアウト

--- app/controllers/admin/searches_controller.rb
+++ app/controllers/admin/searches_controller.rb
@@ -1,6 +1,6 @@
 class Admin::SearchesController < ApplicationController
   def search
     @search_word = params[:search_word]
-    @results = Customer.where("full_name LIKE ?", "%#{@search_word}%")
+    @results = Customer.where("first_name LIKE ? OR last_name LIKE ?", "%#{@search_word}%", "%#{@search_word}%")
   end
 end
